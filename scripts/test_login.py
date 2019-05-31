13598cb8102ea9c8239c6ca92949e3e3fac4a1c4
John Doe <johndoe@example.com>
2019-05-31 16:40:28 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,19 @@
+
+
+class TestLogin:
+
+    def test_login1(self):
+        print("test_login1")
+        assert 1
+
+    def test_login2(self):
+        print("test_login2")
+        assert 0
+
+    def test_login3(self):
+        print("test_login3")
+        assert 1
+
+    def test_login(self):
+        print("test_login4")
+        assert 1
